70adf0af0a0add25d3ed64b17c6d08ab20441985
Yulia Kits <kitsyulia55@gmail.com>
2024-12-06 15:45:03 +0200

    make API call

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <SideMenu />
     <div class="flex-1 p-4 mt-20 flex justify-center items-center w-full">
       <ClientDetails v-if="store.selectedClient" />
+      <div v-else>No clients selected</div>
     </div>
   </div>
 </template>
@@ -14,10 +15,5 @@ import ClientDetails from './components/ClientDetails.vue';
 import './assets/index.css'
 
 const store = useClientStore();
-store.setClients([
-  { id: 1, avatar: 'https://gravatar.com/avatar/f23bd37bf4077b88325f23a2810cae2e?s=400&d=robohash&r=x', first_name: 'John', last_name: 'Doe', email: 'john@example.com', rating: 5 },
-  { id: 2, avatar: 'https://robohash.org/f23bd37bf4077b88325f23a2810cae2e?set=set4&bgset=&size=400x400', first_name: 'Jane', last_name: 'Smith', email: 'jane@example.com', rating: 3 },
-]);
-
 store.loadFromLocalStorage();
 </script>
--- src/components/ClientDetails.vue
+++ src/components/ClientDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="client" class="p-4 bg-white rounded shadow-md">
+  <div class="p-4 bg-white rounded shadow-md">
     <img :src="client.avatar" alt="Avatar" class="w-24 h-24 rounded-full mx-auto" />
     <p class="text-center text-xl font-semibold mt-4">{{ client.first_name }} {{ client.last_name }}</p>
     <p class="text-center text-gray-600">{{ client.email }}</p>

dd5b194b4f53a6962de67bcb27a270b35bd158ef
Yulia Kits <kitsyulia55@gmail.com>
2024-12-07 14:05:15 +0200

    Applied smooth transition for the sidebar

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,19 +1,19 @@
 <template>
-  <div :class="['side-menu cursor-pointer', { 'w-10': isClosed, 'w-72': !isClosed }]" style="transition: width 0.3s;">
-    <div class="p-4 bg-gray-800 text-white flex justify-between" @click="toggleMenu">
+  <div :class="['side-menu cursor-pointer']">
+    <div class="p-4 bg-gray-800 text-white flex justify-between" @click="toggleMenu":class="{ 'w-10': isClosed, 'w-72': !isClosed }" style="transition: width 0.3s;">
       <div></div>
       <div>&#8594;</div>
     </div>
-    <div v-show="!isClosed">
-      <div class="tabs flex justify-between p-4">
+      <div :class="['wrapper',{ 'wrapper__hidden': isClosed }]">
+        <div class="tabs flex justify-between p-4">
         <button @click="selectTab('clients')" :class="['py-2 px-4 rounded-t-lg tab', { 'font-bold bg-gray-200': selectedTab === 'clients', 'bg-gray-100': selectedTab !== 'clients' }]">Clients</button>
         <button @click="selectTab('rating')" :class="['py-2 px-4 rounded-t-lg tab', { 'font-bold bg-gray-200': selectedTab === 'rating', 'bg-gray-100': selectedTab !== 'rating' }]">Rating</button>
+        </div>
+        <div class="content p-4">
+        <ClientList v-if="selectedTab === 'clients'" />
+        <RatingList v-if="selectedTab === 'rating'" />
+        </div>
       </div>
-      <div class="content p-4">
-      <ClientList v-if="selectedTab === 'clients'" />
-      <RatingList v-if="selectedTab === 'rating'" />
-      </div>
-    </div>
   </div>
 </template>
 
@@ -33,8 +33,27 @@ const selectTab = (tab: string) => (selectedTab.value = tab);
 .side-menu {
   transition: width 0.3s;
 }
+.tabs {
+  width: 288px;;
+}
 .tab {
   width: 50%;
+  max-width: 144px;
+}
+.wrapper {
+  overflow: hidden;
+}
+.wrapper.wrapper__hidden {
+  width: 0;
+  transition: 0.3s;
+}
+
+.wrapper:not(.wrapper__hidden) {
+  width: 288px;
+  transition: 0.3s;
+}
+.content {
+  width: 288px;
 }
 </style>
   
